ee123ea8eb8ef856bddb0522d962ec7b5f0096bd
antty <antty@antty.antty>
2025-03-04 11:33:40 +0100

    Entrypoint retries migratons untill done

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,21 @@
 
 #Making Migrations
 echo "Migrating DB"
-alembic upgrade head
+
+# Set the maximum number of retries (optional)
+MAX_RETRIES=7
+RETRY_INTERVAL=5  # Wait time in seconds before retrying
+
+attempt=1
+
+echo "Waiting for database connection..."
+
+while [ $attempt -le $MAX_RETRIES ]; do
+    alembic upgrade head && echo "Migration successful!" && exit 0
+    echo "Database not ready yet. Retrying in $RETRY_INTERVAL seconds... (Attempt: $attempt)"
+    attempt=$((attempt + 1))
+    sleep $RETRY_INTERVAL
+done
 #Starting Server
 echo "Starting Server"
 uvicorn main:app --host 0.0.0.0 --port 5001 --reload
